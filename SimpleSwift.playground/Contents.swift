6d9aef39d560f4d23bb375c7c9bb6476bc1e103a
ting <yinting_p@yahoo.com>
2022-04-06 12:36:09 -0700

    finished

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,11 +1,110 @@
+import Darwin
+import Foundation
+
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    if args.last == "count" {
+        return args.count - 1
+    } else if args.last == "avg" {
+        let nums = args.filter {$0 != "avg"}
+        if(nums.count == 0) {
+            return 0
+        }
+        var sum = 0
+        for num in nums {
+            sum += Int(num)!
+        }
+        let avg = sum / nums.count
+        return avg
+        
+    } else if args.last == "fact" {
+        if(args.count == 1) {
+            return 0
+        }
+        
+        var currNum = Int(args[0])!
+        var total = Int(args[0])!
+        while(currNum != 1) {
+            currNum = currNum - 1
+            total *= currNum
+        }
+        
+        return total
+    } else {
+        let operand = args[1]
+        switch operand {
+        case "+":
+            return Int(args[0])! + Int(args[2])!
+            
+        case "-":
+            return Int(args[0])! - Int(args[2])!
+            
+        case "*":
+            return Int(args[0])! * Int(args[2])!
+            
+        case "/":
+            return Int(args[0])! / Int(args[2])!
+            
+        case "%":
+            return Int(args[0])! % Int(args[2])!
+            
+        default:
+            return -1
+        }
+    }
+    
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    let strArr = arg.split(separator: " ")
+    print(strArr)
+    if strArr.last == "count" {
+        return strArr.count - 1
+    } else if strArr.last == "avg" {
+            let nums = strArr.filter {$0 != "avg"}
+            if(nums.count == 0) {
+                return 0
+            }
+            var sum = 0
+            for num in nums {
+                sum += Int(num)!
+            }
+            let avg = sum / nums.count
+            return avg
+    } else if strArr.last == "fact" {
+        if(strArr.count == 1) {
+            return 0
+        }
+            
+        var currNum = Int(strArr[0])!
+        var total = Int(strArr[0])!
+        while(currNum != 1) {
+            currNum = currNum - 1
+            total *= currNum
+        }
+            
+        return total
+    } else {
+        let oper = strArr[1]
+        
+        switch oper {
+        case "+":
+            return Int(strArr[0])! + Int(strArr[2])!
+           
+        case "-":
+            return Int(strArr[0])! - Int(strArr[2])!
+        
+        case "*":
+            return Int(strArr[0])! * Int(strArr[2])!
+            
+        case "/":
+            return Int(strArr[0])! / Int(strArr[2])!
+            
+        default:
+            return -1
+        }
+    }
 }
 
 // -------------------------------------------
@@ -30,13 +129,13 @@ calculate(["2", "2", "4", "4", "avg"]) == 3
 calculate(["2", "avg"]) == 2
     // 2 / 1 = 2
 calculate(["avg"]) == 0
-    // 0 / 0 = 0 (not really, but it's an edge case
+    // 0 / 0 = 0 (not really, but it's an edge case)
 
-calculate(["0", "fact"]) == 1
 calculate(["1", "fact"]) == 1
 calculate(["2", "fact"]) == 2 // 2*1
 calculate(["5", "fact"]) == 120 // 5*4*3*2*1
 calculate(["fact"]) == 0
+
 
 calculate("2 + 2") == 4
 calculate("2 * 2") == 4
